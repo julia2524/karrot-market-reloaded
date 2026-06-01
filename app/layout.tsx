48ad922313328aa933f5028e115f7d9b0d232478
julia2524 <61957239+julia2524@users.noreply.github.com>
2026-06-01 13:09:22 +0900

    setup karro-market-reloaded

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import { Noto_Sans_KR } from "next/font/google";
+
+const notoSansKr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={`${notoSansKr.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
